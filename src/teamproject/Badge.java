e19401489fd3c4a0bfd3686b61eb42f4e0d37d2d
Joseph L Moses <jmoses2@stu.jsu.edu>
2018-10-11 13:56:09 -0500

    Badge: Corrected Getter/setter

--- src/teamproject/Badge.java
+++ src/teamproject/Badge.java
@@ -9,22 +9,24 @@ public class Badge {
         this.id = id;
         this.description = description;
     }
-	
-    public void setId(String ident) {
-	id = ident;
-        }
 
-    public void setDescription(String desc) {
-	description = desc;
-        }
-	
     public String getId() {
-    	return id;
-	}
-	
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
     public String getDescription() {
-	return description;
-	}
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+	
+    
     @Override
     public String toString(){
         return null;
