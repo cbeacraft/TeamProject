065d2cd6aa1bed39d4f78c14fb2123588a994964
Jacob Battles <jbattles1@stu.jsu.edu>
2018-10-13 19:17:11 -0500

    Edited and cleaned up Getter and Setter methods of Shift class.

--- src/teamproject/Shift.java
+++ src/teamproject/Shift.java
@@ -16,7 +16,7 @@ public class Shift {
         private int lunchdeduct;
 
         //Constructor 
-        public void shift(int id, String description, int starthour, int startminute, int stophour,
+        public Shift(int id, String description, int starthour, int startminute, int stophour,
         int stopminute, int interval, int graceperiod, int dock, int lunchstarthour, int lunchstartminute,
         int lunchstophour, int lunchstopminute, int lunchdeduct) {
 
@@ -28,46 +28,92 @@ public class Shift {
             this.graceperiod = graceperiod;
             this.dock = dock;
             this.lunchstart = LocalTime.of(lunchstarthour, lunchstartminute);
-            this.lunchstop = LocalTime.of(lunchstarthour, lunchstartminute);
+            this.lunchstop = LocalTime.of(lunchstophour, lunchstopminute);
             this.lunchdeduct = lunchdeduct;
 
         }
+    //Setter Methods
+    public void setId(int id) {
+        this.id = id;
+    }
 
-        //Getter methods
-        public int getId() {
-                return id; 
-        }
-        public String getDescription() {
-                return description;
-        }
-        public LocalTime getStart() {
-                return start;
-        }
-        public LocalTime getStop() {
-                return stop;
-        }
-        public int getInterval() {
-                return interval;
-        }
-        public int getGraceperiod() {
-                return graceperiod;
-        }
-        public int getDock() {
-                return dock;
-        }
-        public LocalTime getLunchstart() {
-                return lunchstart;
-        }
-        public LocalTime getLunchstop() {
-                return lunchstop;
-        }
-        public int getLunchdeduct() {
-                return lunchdeduct;
-        }
-        @Override
-        public String toString() {
-                return null;
-        }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setStart(LocalTime start) {
+        this.start = start;
+    }
+
+    public void setStop(LocalTime stop) {
+        this.stop = stop;
+    }
+
+    public void setInterval(int interval) {
+        this.interval = interval;
+    }
+
+    public void setGraceperiod(int graceperiod) {
+        this.graceperiod = graceperiod;
+    }
+
+    public void setDock(int dock) {
+        this.dock = dock;
+    }
+
+    public void setLunchstart(LocalTime lunchstart) {
+        this.lunchstart = lunchstart;
+    }
+
+    public void setLunchstop(LocalTime lunchstop) {
+        this.lunchstop = lunchstop;
+    }
+
+    public void setLunchdeduct(int lunchdeduct) {
+        this.lunchdeduct = lunchdeduct;
+    }
+    
+    //Getter Mrthods
+    public int getId() {
+        return id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public LocalTime getStart() {
+        return start;
+    }
+
+    public LocalTime getStop() {
+        return stop;
+    }
+
+    public int getInterval() {
+        return interval;
+    }
+
+    public int getGraceperiod() {
+        return graceperiod;
+    }
+
+    public int getDock() {
+        return dock;
+    }
+
+    public LocalTime getLunchstart() {
+        return lunchstart;
+    }
+
+    public LocalTime getLunchstop() {
+        return lunchstop;
+    }
+
+    public int getLunchdeduct() {
+        return lunchdeduct;
+    }
+    
 }
 
 
