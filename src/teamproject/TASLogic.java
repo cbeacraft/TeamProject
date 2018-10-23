06a51be33381a4d6a6d85da569f19cc831e1d881
keyenna <buesry@gmail.com>
2018-10-23 14:07:04 -0500

    Feature 4 structure
    Added the start of feature 4. Create TASLogic class.

---
+++ src/teamproject/TASLogic.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package teamproject;
+import java.util.*;
+//import java.sql.*;
+/**
+ *
+ * @author Brooke
+ */
+public class TASLogic {
+    
+    
+    public static int calculateTotalMinutes(ArrayList<Punch> dailypunchlist, Shift shift){
+      return 1;  
+    }
+    
+    public void testMinutesAccruedShift1Weekday() {
+        
+    }
+    
+     public void testMinutesAccruedShift1WeekdayWithTimeout() {
+         
+    }
+     
+     public void testMinutesAccruedShift1Weekend() {
+         
+    }
+     
+     public void testMinutesAccruedShift2Weekday() {
+         
+    }
+}
